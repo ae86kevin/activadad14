b7433385cac8e9c23f03d7047be3cbaa9a5a3bd1
ltkfa <ae86mm@gmail.com>
2025-08-09 21:33:25 -0600

    update4

--- main.py
+++ main.py
@@ -1,42 +1,53 @@
 participantes ={}
 
-cantidad=int(input("Ingrese la cantidad de participantes: "))
-for i in range(cantidad):
-    print(f"\nIngrese los datos de participante: {i+1}")
-    numeroDorsal=int(input("Ingrese el numero de dorsal: "))
-    participantes[numeroDorsal] = {}
-
-    participantes[numeroDorsal]["nombre"]=input("Ingrese el nombre del dorsal: ")
-    participantes[numeroDorsal]["edad"]=int(input("Ingrese el edad: "))
-    participantes[numeroDorsal]["categoria"]=input("Ingrese el categoria: ")
-
-
-def quick_sorter(lista):
-    if len(lista) <= 1:
-        return lista
-
-    pivote = lista[0]
-    menores =[x for x in lista[1:] if x[1]["edad"] <= pivote[1]["edad"]]
-    mayores= [x for x in lista[1:] if x[1]["edad"] > pivote[1]["edad"]]
-
-    return quick_sorter(menores) + [pivote] + quick_sorter(mayores)
-
-lista_participantes =list(participantes.items())
-
-
-
-
-
 print("Menu principal")
 
-seleccion =""
+def quic_sorter(lista):
+    if len(lista) == 0:
+        return lista
+
+    pivot = lista[0]
+    menor =[x for x in lista[:1] if x[1]["edad"] <= pivot[1]["edad"]]
+    mayor =[x for x in lista[:1] if x[1]["edad"] > pivot[1]["edad"]]
+
+    return quic_sorter(menor) + [pivot] + quic_sorter(mayor)
+
+lista_participantes=list(participantes.items())
+participantes_ordenados=quic_sorter(lista_participantes)
+
+
+seleccion = ""
 while seleccion != "0":
     print("\n1.Agregar participante")
     print("2.MOstrar particiapantes por edad")
     print("3.mostarr particiapantes por nombre")
     print("0. salir")
-    seleccion=int(input())
+    seleccion = int(input())
 
+    match seleccion:
+        case 1:
+            cantidad=int(input("\nIngrese la cantidad de participantes: "))
+            for i in range(cantidad):
+                print(f"\nIngrese la informacion del candidato: {i+1}")
+                dorsal=int(input("Ingrese el numero dorsal: "))
+                participantes[dorsal]={}
+                participantes[dorsal]["nombre"]=int("Ingres el nombre: ")
+                participantes[dorsal]["edad"]=int(input("Ingrese la edad: "))
+                participantes[dorsal]["categoria"]=input("Ingrese la categoria: ")
+
+
+            print("Participante registrada exitosamente")
+
+        case "2":
+            print("Participantes ordenados por nombre")
+            for i,info in participantes_ordenados:
+                print(f"Dorsal: {i}, Nombre: {info['nombre']}, Edad: {info['edad']}, Categoris: {info['categoria']}")
+
+
+
+
+        case "3":
+            print("Participantes ordenados por edad")
 
 
 
