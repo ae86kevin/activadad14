ff5e4920107386e7cf1709dfaa2d4d5431c1636c
ltkfa <ae86mm@gmail.com>
2025-08-09 22:15:30 -0600

    update4

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ participantes ={}
 print("Menu principal")
 
 def quic_sorter(lista):
-    if len(lista) == 0:
+    if len(lista) <=1:
         return lista
 
     pivot = lista[0]
-    menor =[x for x in lista[:1] if x[1]["edad"] <= pivot[1]["edad"]]
-    mayor =[x for x in lista[:1] if x[1]["edad"] > pivot[1]["edad"]]
+    mayor =[x for x in lista[1:] if x[1]["edad"] >= pivot[1]["edad"]]
+    menor =[x for x in lista[1:] if x[1]["edad"] < pivot[1]["edad"]]
 
+    return quic_sorter(mayor) + [pivot] + quic_sorter(menor)
+
+def quick_sorterN(lista):
+    if len(lista) <=1:
+        return lista
+    pivot = lista[0]
+    menor=[x for x in lista[1:] if x[1]["nombre"] <= pivot[1]["nombre"]]
+    mayor=[x for x in lista[1:] if x[1]["nombre"] > pivot[1]["nombre"]]
     return quic_sorter(menor) + [pivot] + quic_sorter(mayor)
-
-lista_participantes=list(participantes.items())
-participantes_ordenados=quic_sorter(lista_participantes)
-
 
 seleccion = ""
 while seleccion != "0":
@@ -28,20 +32,23 @@ while seleccion != "0":
         case 1:
             cantidad=int(input("\nIngrese la cantidad de participantes: "))
             for i in range(cantidad):
-                print(f"\nIngrese la informacion del candidato: {i+1}")
-                dorsal=int(input("Ingrese el numero dorsal: "))
+                print(f"\nIngrese los datos de participante {i+1}:")
+                dorsal=int(input("ingrese NO. dorsal: "))
                 participantes[dorsal]={}
-                participantes[dorsal]["nombre"]=int("Ingres el nombre: ")
+                participantes[dorsal]["nombre"]=input("Ingres el nombre: ")
                 participantes[dorsal]["edad"]=int(input("Ingrese la edad: "))
                 participantes[dorsal]["categoria"]=input("Ingrese la categoria: ")
 
 
             print("Participante registrada exitosamente")
 
-        case "2":
+        case 2:
             print("Participantes ordenados por nombre")
-            for i,info in participantes_ordenados:
-                print(f"Dorsal: {i}, Nombre: {info['nombre']}, Edad: {info['edad']}, Categoris: {info['categoria']}")
+            lista_participantes = list(participantes.items())
+            participantes_ordenados = quic_sorter(lista_participantes)
+
+            for dorsal,info in participantes_ordenados:
+                print(f"Dorsal: {dorsal}, Nombre: {info['nombre']}, Edad: {info['edad']}, Categoris: {info['categoria']}")
 
 
 
